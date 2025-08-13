e01c8f15d68db82e7c66392b311c6a2094aeb8d0
happytilt <112998219+happytilt@users.noreply.github.com>
2025-08-13 11:16:28 -0700

    Add files via upload

---
+++ WhatAreU.py
@@ -0,0 +1,57 @@
+import sys
+
+print('\n------------------------')
+print('  WhatAreU.py')
+print('  Script by happytilt')
+print('------------------------')
+
+#Handles command arguements
+if len(sys.argv) != 3:
+    print("Usage: python WhatAreU.py <scheme-file-path> <string>")
+    sys.exit(1)
+
+#Converts string to all uppercase
+string = sys.argv[2].upper()
+print(f'Before: {string}')
+
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+
+#Defines section_lengths and creates dictionary from file_path
+def LoadTextFile(file_path):
+	with open(file_path, 'r') as file:
+		lines = file.read().splitlines()
+	lines = [line.strip() for line in lines if line.strip() and not line.strip().startswith('#')]
+	section_lengths = list(map(int, lines[0].split(',')))
+	dictionary = {}
+	for line in lines[1:]:
+		if '=' in line:
+			key, value = line.split('=', 1)
+			dictionary[key.strip()] = value.strip()
+	return section_lengths, dictionary
+
+#Cuts string up, as defined by section_lengths
+def Slice(string, section_lengths):
+	index = 0
+	sliced_sections = []
+	for i in section_lengths:
+		sliced = string[index:index+i]
+		index += i
+		sliced_sections.append(sliced)
+	return sliced_sections
+
+#Translates sliced_sections using dictionary into full_translation
+def Translate(sliced_sections, dictionary):
+	translated_sections = []
+	for i in sliced_sections:
+		sliced_sections = dictionary.get(i,i)
+		translated_sections.append(sliced_sections)
+	full_translation = ' '.join(translated_sections)
+	return full_translation
+
+section_lengths, dictionary = LoadTextFile(file_path)
+sliced_sections = Slice(string, section_lengths)
+full_translation = Translate(sliced_sections, dictionary)
+
+print(f'After: {full_translation}')
+print('------------------------\n')
