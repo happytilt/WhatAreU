8d2d3c6858e9aa6d382b0a98c32ae02799c16727
happytilt <h4ppytilt@proton.me>
2025-08-13 16:31:57 -0700

    update

--- WhatAreU.py
+++ WhatAreU.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 print('\n------------------------')
 print('  WhatAreU.py')
@@ -23,31 +24,40 @@ def LoadTextFile(file_path):
 		lines = file.read().splitlines()
 	lines = [line.strip() for line in lines if line.strip() and not line.strip().startswith('#')]
 	section_lengths = list(map(int, lines[0].split(',')))
+
 	dictionary = {}
 	for line in lines[1:]:
 		if '=' in line:
 			key, value = line.split('=', 1)
 			dictionary[key.strip()] = value.strip()
+
 	return section_lengths, dictionary
 
 #Cuts string up, as defined by section_lengths
 def Slice(string, section_lengths):
-	index = 0
 	sliced_sections = []
+	try:
+		#replaced for loop with regex matching
+		regex_pattern = '^' + ''.join(f'(.{{{length}}})' for length in section_lengths) + "$"
+		sliced_sections = re.match(regex_pattern, string).groups()
+	except:
+		print('\n* Sectioning error, invalid string or scheme file error')
+		print('------------------------\n')
+		exit(1)
+	'''
+	#index = 0
 	for i in section_lengths:
 		sliced = string[index:index+i]
 		index += i
 		sliced_sections.append(sliced)
+	'''
 	return sliced_sections
 
 #Translates sliced_sections using dictionary into full_translation
+
 def Translate(sliced_sections, dictionary):
-	translated_sections = []
-	for i in sliced_sections:
-		sliced_sections = dictionary.get(i,i)
-		translated_sections.append(sliced_sections)
-	full_translation = ' '.join(translated_sections)
-	return full_translation
+    translated_sections = [dictionary.get(i) or (print(f'\n* Dict Error: {i} not found\n') or f'({i})?') for i in sliced_sections]
+    return ' '.join(translated_sections)
 
 section_lengths, dictionary = LoadTextFile(file_path)
 sliced_sections = Slice(string, section_lengths)
